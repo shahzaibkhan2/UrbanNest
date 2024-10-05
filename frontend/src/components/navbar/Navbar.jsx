b9e2149c36cd9a52a178fddd3b31c0c6ab7bc302
shahzaibkhan2 <drshahzeb47@gmail.com>
2024-10-05 17:11:52 +0500

    navbar changes

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,13 +1,16 @@
 import { IoSearchSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header className="fixed top-0 bg-yellow-300 shadow-md w-full">
-      <div className="max-w-[1336px] mx-auto flex justify-between items-center h-20 px-4">
-        <h1 className="text-xl sm:text-2xl font-bold flex flex-nowrap">
-          <span className="text-blue-700">Urban</span>
-          <span className="text-blue-900">Nest</span>
-        </h1>
+      <nav className="max-w-[1336px] mx-auto flex justify-between items-center h-20 px-4">
+        <Link to="/">
+          <h1 className="text-xl sm:text-2xl font-bold flex flex-nowrap">
+            <span className="text-blue-700">Urban</span>
+            <span className="text-blue-900">Nest</span>
+          </h1>
+        </Link>
         <form className="flex items-center bg-yellow-200 rounded-lg p-3">
           <input
             type="text"
@@ -16,7 +19,18 @@ const Navbar = () => {
           />
           <IoSearchSharp className="text-blue-900 size-6" />
         </form>
-      </div>
+        <ul className="flex gap-3 text-md sm:text-lg text-blue-900 font-semibold">
+          <Link to="/home">
+            <li className="hidden sm:inline cursor-pointer">Home</li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline cursor-pointer">About</li>
+          </Link>
+          <Link to="/login">
+            <li className="cursor-pointer">Login</li>
+          </Link>
+        </ul>
+      </nav>
     </header>
   );
 };
