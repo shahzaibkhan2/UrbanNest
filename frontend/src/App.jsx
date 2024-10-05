2fd83637242d700c372ebf8bfe6162fc529eb62e
shahzaibkhan2 <drshahzeb47@gmail.com>
2024-10-05 16:26:44 +0500

    UrbanNest

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { About, Home, Login, Profile, Register } from "./pages";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
