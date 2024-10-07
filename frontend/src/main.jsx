b92c7068c445c727e312632bea106a92cc47f58f
shahzaibkhan2 <drshahzeb47@gmail.com>
2024-10-07 17:27:19 +0500

    Navigation changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import AuthContextProvider from "./context/AuthContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import mainStore from "./store/index.js";
